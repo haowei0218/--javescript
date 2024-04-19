cc117e57556fd9cf46dabc4caa055134f3e0e00b
haowei0218 <haoweizhou891@gmail.com>
2024-04-19 10:33:04 +0800

    實現新增 刪除 查詢功能

---
+++ function.js
@@ -0,0 +1,130 @@
+let CashList = [];
+const TestList = [];
+const ImgList = ["./image/1.png", "./image/2.png", "./image/3.png"];
+const Container = document.querySelector(".container");
+const ChangeBtn = document.querySelector(".img_change");
+const AddBtn = document.querySelector(".add");
+const DeleteBtn = document.querySelector(".delete");
+const EnterBtn = document.querySelector(".aaa");
+const EnterInputBox = document.getElementById("itemNumber");
+const SearchBox = document.querySelector(".search");
+const SearchBtn = document.querySelector(".search-btn");
+const DeleteAllBtn = document.querySelector(".delete-all");
+
+//隨機數字
+function RandomNumber(RandomValue) {
+  const number = Math.random();
+  const TestRandom = number * RandomValue;
+  const TestNumber = Math.ceil(TestRandom);
+  return TestNumber;
+}
+
+//隨機產生多組資料
+function RanDomCashAssets(Count = Number) {
+  for (let i = 0; i < Count; i++) {
+    const Id = Number(RandomNumber(1000));
+    const Name = "test" + RandomNumber(1000000);
+    const Currency = "USD";
+    const Balance = RandomNumber(100000);
+    const AssetsData = { Id, Name, Currency, Balance };
+    CashList.push(AssetsData);
+    console.log(CashList);
+  }
+  console.log(CashList);
+}
+
+//新增資料按鈕
+function AddInformation() {
+  RanDomCashAssets(1);
+  Content();
+}
+//新增按鈕
+AddBtn.addEventListener("click", AddInformation);
+
+//刪除資料
+function DeleteInformation() {
+  const ListLength = CashList.length;
+  if (ListLength > 0) {
+    CashList.pop();
+    Content();
+    console.log(CashList);
+  } else {
+    alert("Can not delete");
+  }
+}
+//刪除按鈕
+DeleteBtn.addEventListener("click", DeleteInformation);
+
+//在頁面上顯示資訊
+function Content() {
+  const content = CashList.map((item) => {
+    return `
+    <ul class="items">
+        <li class="item">${item.Id}</li>
+        <li class="item">${item.Name}</li>
+        <li class="item">${item.Currency}</li>
+        <li class="item">${item.Balance}</li>
+    </ul>
+    `;
+  }).join(" ");
+
+  Container.innerHTML = content;
+}
+
+//在輸入匡輸入一個數字 然後根據輸入的數字產生出資料
+function getInfo() {
+  const Value = EnterInputBox.value;
+  RanDomCashAssets(Value);
+  Content();
+}
+//輸入按鈕
+EnterBtn.addEventListener("click", getInfo);
+
+//搜尋資料功能
+function Search() {
+  const Value = Number(SearchBox.value);
+  for (let item = 0; item < CashList.length; item++) {
+    //處理不匹配
+    if (CashList[item].Id !== Value) {
+      console.log(CashList[item], "Not match");
+      continue;
+    }
+  }
+  //檢查輸入的值是否為空
+  if (Value !== 0) {
+    console.log(typeof Value);
+    if (CashList.length === 0) {
+      alert("data not have result");
+    }
+  } else {
+    alert("enter the id");
+  }
+
+  //處理篩選資料
+  const result = CashList.filter((item) => item.Id === Value)
+    .map((item) => {
+      console.log(item);
+      return `
+    <ul class="items">
+    <li class="item">${item.Id}</li>
+    <li class="item">${item.Name}</li>
+    <li class="item">${item.Currency}</li>
+    <li class="item">${item.Balance}</li>
+</ul>
+`;
+    })
+    .join("");
+  Container.innerHTML = result;
+
+  //console.log(result);
+}
+
+SearchBtn.addEventListener("click", Search);
+
+function DeleteALL() {
+  CashList = [];
+  Content();
+  console.log(CashList);
+}
+
+DeleteAllBtn.addEventListener("click", DeleteALL);
