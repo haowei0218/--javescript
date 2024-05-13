8605fd8f61e32eb272a464f9e37a52d0c3787880
white <haoweizhou891@gmail.com>
2024-05-13 17:01:28 +0800

    api with url參數

--- 幫期末project測試函數/index.js
+++ 幫期末project測試函數/index.js
@@ -1,29 +1,37 @@
 const TestBtn = document.querySelector(".test");
 
 TestBtn.addEventListener("click", () => {
-  SearchBooks();
+  TestBook();
 });
 /**
  * @async
- * @function SearchBooks() - get all books data in supabase
+ * @function SearchBooks() 非同步函數 該函數向api發送request 從資料庫獲取所有資料
  * @param {string} url - 自定義的api
  * @return {JSON}
  */
 async function SearchBooks() {
-  fetch("/api/All_books", {
-    method: "GET",
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("NetWork response have error");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      displayData(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  try {
+    const response = await fetch("http://localhost:3000/BOOK");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/**
+ * @async 非同步函數 向後端發送一個request 後端從url解析出bookId後在資料庫進行查找
+ * @param {*} userId
+ * @param {*} bookId
+ * @return {void}
+ */
+async function TestBook(userId, bookId) {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/users/${userId}/books/${bookId}`
+    );
+    console.log(await response.json());
+  } catch (error) {
+    console.log(error);
+  }
 }
