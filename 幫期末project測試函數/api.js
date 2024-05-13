069dd15b238dac4a4edd1ccf768dd4358da2f712
haowei0218 <137982945+haowei0218@users.noreply.github.com>
2024-05-13 17:07:38 +0800

    Update api.js

--- 幫期末project測試函數/api.js
+++ 幫期末project測試函數/api.js
@@ -1,16 +1,15 @@
 /**導入後端框架expresss */
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 /**導入supabaseAPI and supabaseURL */
-const {createClient} = require('@supabase/supabase-js');
+const { createClient } = require("@supabase/supabase-js");
 const supabase_url = "https://csutzncxyvmaoeujpols.supabase.co";
 const supabase_api =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzdXR6bmN4eXZtYW9ldWpwb2xzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQzNTg2MDgsImV4cCI6MjAyOTkzNDYwOH0.BjiNueEyc3r7Wcu-K4HPoZrb23vc0vSz2P9ZCfPJYnQ";
-const supabase = createClient(supabase_url,supabase_api)
-
+const supabase = createClient(supabase_url, supabase_api);
 
 /**創建HTTP伺服器 */
 const http = require("http");
@@ -19,16 +18,14 @@ const server = http.createServer((req, res) => {
   res.end("hello world\n");
 });
 
-
-
 /**
  * 定義一個GET路由 取得所有書籍的資料
  * @route GET /api/books
  * @returns {object[]} books - all data in supabase
  * @returns 將response以json的格式回傳
  * @returns catch(error) => res.status(500)
- * 
- * 提醒:每次改code前 先停止後端運作 改完後再啟動 
+ *
+ * 提醒:每次改code前 先停止後端運作 改完後再啟動
  */
 
 app.get("/BOOK", async (req, res) => {
@@ -51,6 +48,15 @@ app.get("/api/books/:book_name", async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: "查詢書名時發生錯誤" });
   }
+});
+app.get("/users/:userId/books/:bookId", function (req, res) {
+  const userId = req.params.userId;
+  const bookId = req.params.bookId;
+
+  res.json({
+    userId: userId,
+    bookId: bookId,
+  });
 });
 const PORT = 3000;
 app.listen(PORT, () => {
