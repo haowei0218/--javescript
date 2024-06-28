7f2f3068aa41ff7d81031cf149d73978fdcdc11a
white <haoweizhou891@gmail.com>
2024-06-28 17:10:47 +0800

    feat:刷題

--- javascript/js with qzit/array1.js
+++ javascript/js with qzit/array1.js
@@ -155,3 +155,239 @@ function SeriesSum(n) {
          return result.toFixed(2)
 }
 console.log(SeriesSum(3))
+
+function sumTwoSmallestNumbers(numbers) {
+         for (let i = 0; i < numbers.length; i++) {
+                  for (let j = 0; j < numbers.length; j++) {
+                           if (numbers[j] > numbers[j + 1]) {
+                                    const temp = numbers[j + 1];
+                                    numbers[j + 1] = numbers[j];
+                                    numbers[j] = temp;
+                           }
+                  }
+         }
+         return numbers[0] + numbers[1];
+}
+
+console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]))
+
+function accum(str) {
+         const result = str.split('');
+         let finalArray = [];
+         let count = 0;
+         for (let i = 0; i < result.length; i++) {
+                  count += 1
+                  let str1 = ''
+                  if (i === 0) {
+                           finalArray.push(result[0].toUpperCase())
+                           continue;
+                  }
+                  for (let j = 0; j < count; j++) {
+                           if (j === 0) {
+                                    str1 += result[i].toUpperCase();
+                           } else {
+                                    str1 += result[i].toLowerCase()
+                           }
+
+                  }
+                  finalArray.push("-" + str1);
+         }
+         console.log(finalArray.join(''))
+}
+let str1 = '';
+for (let i = 0; i < 4; i++) {
+         const str = 'i';
+         console.log(str1 += str);
+}
+console.log(accum("ZpglnRxqenU"))
+
+function getSum(a, b) {
+         let count = 0
+         if (a > b) {
+                  for (let i = b; i <= a; i++) {
+                           count += i
+                           console.log(i)
+                  }
+         } else {
+                  for (let i = a; i <= b; i++) {
+                           count += i
+                  }
+         }
+
+         return count
+}
+
+console.log(getSum(0, -1))
+
+const rps = (p1, p2) => {
+         if (p1 === p2) {
+                  return "Draw!";
+         }
+
+         if (p1 === "scissors" && p2 === "paper" ||
+                  p1 === "paper" && p2 === "rock" ||
+                  p1 === "rock" && p2 === "scissors") {
+                  return "Player 1 won!";
+         } else {
+                  return "Player 2 won!";
+         }
+};
+
+console.log(rps('rock', 'scissors'))
+
+function countPositivesSumNegatives(input) {
+         if (!Array.isArray(input)) {
+                  return [];
+         }
+         if (input.length === 0) {
+                  return [];
+         } else {
+                  let final = [];
+                  let OneArray = [];
+                  let TwoArray = [];
+
+                  for (let i = 0; i < input.length; i++) {
+                           input[i] > 0 ? OneArray.push(input[i]) : TwoArray.push(input[i]);
+                  }
+                  final.push(OneArray.length)
+                  final.push(TwoArray.reduce((a, b) => a + b, 0))
+                  return final
+         }
+
+}
+console.log(countPositivesSumNegatives(null))
+
+console.log(typeof [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
+function invert(array1) {
+         let final = [];
+         for (let i = 0; i < array1.length; i++) {
+                  array1[i] > 0 ? final.push(0 - array1[i]) : final.push(array1[i] - (2 * array1[i]));
+         }
+         return final
+
+}
+console.log(invert([1, -2, 3, -4, 5]))
+
+var countSheep = function (num) {
+         //your code here
+         let sheepArray = [];
+         let sheep = ""
+         for (let i = 1; i <= num; i++) {
+                  sheep += `${i} sheep...`;
+         }
+         return sheep
+}
+console.log(countSheep(4))
+
+function duplicateCount(text) {
+         //...
+         let count = new Set();
+         let textArr = text.toLowerCase().split('')
+         textArr.sort();
+         for (let i = 0; i < textArr.length; i++) {
+                  for (let j = 0; j < textArr.length; j++) {
+                           if (textArr[j] === textArr[j + 1]) {
+                                    count.add(textArr[j]);
+                           }
+                  }
+         }
+         return count.size
+}
+console.log(duplicateCount("Indivisibilities"))
+
+
+function digPow(n, p) {
+         // ...
+         let count = 0;
+         let arrayN = n.toString().split('')
+         console.log(arrayN)
+         let arrayNN = arrayN.map(item => Number(item));
+         for (let i = 0; i < arrayNN.length; i++) {
+                  console.log(Math.pow(arrayNN[i], i + p));
+                  count += Math.pow(arrayNN[i], i + p)
+         }
+         return count % n === 0 ? count / n : -1;
+}
+console.log(digPow(92, 1))
+
+function printerError(s) {
+         const result = s.replace(/[nopqrstuvwxyz]/g, "*").split('')
+         let count = 0;
+         for (let i = 0; i < result.length; i++) {
+                  if (result[i] === "*") { count += 1 }
+         }
+         return `${count}/${s.split('').length}`;
+}
+console.log(printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"))
+
+
+
+function bmi(weight, height) {
+         const bmi = weight / Math.pow(height, 2)
+         bmi.toFixed(2)
+         if (bmi <= 18.5) {
+                  return 'Underweight';
+         } else if (bmi <= 25.0) {
+                  return 'Normal';
+         } else {
+                  return "Obese"
+         }
+
+}
+console.log(bmi(80, 1.80))
+
+function likes(names) {
+         // TODO
+         if (names.length > 3) {
+                  return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+         } else if (names.length === 1) {
+                  return names.join(' ') + " " + "likes this"
+         } else if (names.length === 2) {
+                  return names.join(' and ') + " " + "like this"
+         } else if (names.length === 3) {
+                  return `${names[0]}, ${names[1]} and ${names[2]} like this`
+         }
+         else {
+                  return 'no one likes this'
+         }
+}
+console.log(likes(['Jacob', 'Alex']))
+
+function addBinary(a, b) {
+         const result = parseInt(a + b);
+         return result.toString(2);
+}
+console.log(addBinary(1, 1))
+
+function findSmallestInt(arr) {
+         //your code here
+         arr.sort((a, b) => a - b)
+         return arr[0];
+}
+console.log(findSmallestInt([8, 25, 12, 78, 1]))
+
+function points(games) {
+         let count = 0;
+         games.forEach((item) => {
+                  item.split(":")
+                  console.log(Number(item[0]));
+                  console.log(Number(item[2]));
+                  if (Number(item[0]) === Number(item[2])) {
+                           count += 1
+                  } else if (Number(item[0]) < Number(item[2])) {
+                           count += 0
+                  } else {
+                           count += 3
+                  }
+
+         })
+         return count
+}
+console.log(points(["1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4"]))
+
+function solution(string) {
+         return "";
+}
+const a = 'abc'
+
+console.log('abc' === 'ABC' ? true : false);
--- 期末project測試函數/index.js
+++ 期末project測試函數/index.js
@@ -1,460 +1,196 @@
-const TestBtn = document.querySelector(".test");
-const DisplayContainer = document.querySelector(".data_table");
-const id = document.getElementById("id");
-const name_btn = document.getElementById("name");
-const author = document.getElementById("author");
-const class_btn = document.getElementById("classification");
-const add_btn = document.querySelector(".test_with_create");
-const create_btn = document.querySelector(".create_classification");
-const create_container = document.querySelector(".create_info");
-const createInfo = document.querySelector(".create_content");
-const overlay = document.querySelector(".overlay");
-const loading = document.querySelector(".loading");
-const page1 = document.querySelector(".perpage-1");
-const page2 = document.querySelector(".perpage-2");
-const page3 = document.querySelector(".perpage-3");
-const page4 = document.querySelector(".perpage-4");
-const Classification = document.querySelector(".classification");
-const test_borrow = document.querySelector(".test_borrow");
-const search_btn = document.querySelector(".search");
-const FilterSelect = document.querySelector(".filter_info");
-const DownButton = document.querySelector(".down");
-const testBlock = document.querySelector(".testBlock");
-/**-----------------------------------------------------------
- * 測試用的功能
- */
-function DataRandom(result) {
-  const Rnumber = Math.random();
-  const random = Rnumber * result;
-  const testNum = Math.ceil(random);
-  return testNum;
+const string1 = "hello world";
+console.log(string1.indexOf(" "));
+
+function abbrevName(name) {
+  const stringIndex = name.indexOf(" ");
+  const Final = name[0] + "." + name[stringIndex + 1];
+  return Final.toUpperCase();
 }
 
-function ID() {
-  const Id = String(DataRandom(10000000));
-  return Id;
+console.log(abbrevName("hello world"));
+
+function NumberToString(num) {
+  const stringNum = String(num).toString();
+  return stringNum;
 }
+console.log(typeof NumberToString(123));
 
-function BookName() {
-  const Name = String("Book" + DataRandom(1000000));
-  return Name;
+function removeExclamationMarks(s) {
+  if (s.indexOf("!")) {
+    return s.replace("!", "");
+  } else {
+    return s;
+  }
 }
+console.log(removeExclamationMarks("Hello world!"));
+console.log(removeExclamationMarks("Helloworld"));
 
-function Author_name() {
-  const Author = String("Author" + DataRandom(10000000));
-  return Author;
-}
-
-function classification() {
-  const classList = [
-    "文學",
-    "藝術",
-    "人文",
-    "歷史",
-    "體育",
-    "奇幻",
-    "武俠",
-    "漫畫",
-    "雜誌",
-    "愛情",
-    "恐怖",
-  ];
-  const number = DataRandom(9);
-  console.log(number);
-  return classList[number];
-}
-
-add_btn.addEventListener("click", () => {
-  CreateBooksData();
-});
-
-async function CreateBooksData() {
-  const id = ID();
-  const book_name = BookName();
-  const author_name = Author_name();
-  const Classification = classification();
-
-  const Apidata = await FetchApi(
-    `http://localhost:4050/api/id/${id}/name/${book_name}/author/${author_name}/class/${Classification}`,
-    "POST"
-  );
-  console.log(Apidata);
-  DisplayBookInfo(Apidata);
-}
-
-/**---------------------------------------------------------- */
-/**
- * @async
- * @function DisplayContent 在頁面顯示書本的基本資料
- * @param {Array} database
- */
-async function DisplayBookInfo(database) {
-  const displayInHtml = Object.values(database)
+function squareDigits(num1) {
+  const result = String(num1).split("");
+  const final = result
     .map((item) => {
-      const { book_id, book_name, author_name, classification } = item;
-      return `
-    <ul>
-      <li>${book_id}</li>
-      <li>${book_name}</li>
-      <li>${author_name}</li>
-      <li>${classification}</li>
-      <button class="${book_id}-btn" width="20px" height="10px">刪除</button>
-    </ul>
-    
-    `;
+      return String(Number(item) ** 2);
     })
-    .join(" ");
-  DisplayContainer.innerHTML = displayInHtml;
+    .join("");
+  return final;
 }
-/**
- * @async
- * @function DisplayBorrowInfo 在頁面顯示借閱資料
- */
-async function DisplayBorrowInfo(borrowData) {
-  const displayInHtml = Object.values(borrowData)
-    .map((item) => {
-      const { record_id, book_id, user_id, borrow_status, borrow_date } = item;
-      return `
-    <ul>
-      <li>${record_id}</li>
-      <li>${book_id}</li>
-      <li>${user_id}</li>
-      <li>${borrow_status}</li>
-      <li>${borrow_date}</li>
-      <button class="${book_id}-btn" width="20px" height="10px">刪除</button>
-    </ul>
-    `;
-    })
-    .join(" ");
-  DisplayContainer.innerHTML = displayInHtml;
-}
+console.log(squareDigits(1234));
 
-/**
- * @async
- * @function FetchApi 串接api 發送request
- * @param {string} url
- * @returns {JSON}
- */
-async function FetchApi(url, method) {
-  try {
-    const response = await fetch(url, { method: `${method}` });
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(`${error}`);
-  }
-}
-
-/**
- * @async
- * @function SearchBooks() 該函數向api發送request 從資料庫獲取所有資料
- * @param {string} url - 自定義的api
- * @return {JSON}
- */
-async function SearchAllBooks() {
-  try {
-    const Apidata = await FetchApi(
-      "http://localhost:4050/api/all_books",
-      "GET"
-    );
-    DisplayBookInfo(Apidata);
-    console.log(Apidata);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-/**
- * 用於在網頁執行動作時顯示動畫
- * @function DisplayLoading
- */
-function DisplayLoading() {
-  loading.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-}
-/**
- * 用於執行完動作時隱藏動畫
- * @function HiddenLoading
- */
-function HiddenLoading() {
-  loading.classList.add("hidden");
-  overlay.classList.add("hidden");
-}
-
-/**
- * @async
- * @function SearchBooksId 查詢書本編號
- * @param {string} bookId
- * @return {void}
- */
-async function SearchBooksId() {
-  try {
-    const book_id = input_id.value;
-    console.log(book_id);
-    const Apidata = await FetchApi(
-      `http://localhost:4050/api/books_id/${book_id}`,
-      "GET"
-    );
-    DisplayBookInfo(Apidata);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-/**
- * @async
- * @function SearchBookName 查詢書本名稱
- * @returns {void}
- */
-async function SearchBookName() {
-  try {
-    const name = input_name.value;
-    console.log(name);
-    const Apidata = await FetchApi(
-      `http://localhost:4050/api/book_name/${name}`,
-      "GET"
-    );
-    DisplayBookInfo(Apidata);
-  } catch (error) {
-    alert(`${error}`);
-  }
-}
-
-/**
- * @async
- * @function ManualCreateBook - 手動新增書籍的基本資料
- * @returns {void}
- */
-async function ManualCreateBook() {
-  try {
-    const result_id = id.value;
-    const result_name = name_btn.value;
-    const result_author = author.value;
-    const result_classification = class_btn.value;
-    console.log(result_author);
-    const Apidata = await FetchApi(
-      `http://localhost:4050/api/id/${result_id}/name/${result_name}/author/${result_author}/class/${result_classification}`,
-      "POST"
-    );
-    DisplayBookInfo(Apidata);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-create_btn.addEventListener("click", async () => {
-  await ManualCreateBook();
-});
-
-/**
- * @async
- * @function CreateInfo() 點擊後再網頁中央顯示輸入匡用於新增資料
- * @param {string} title
- */
-async function CreateInfo(title) {
-  create_container.classList.remove("hidden");
-  create_container.innerHTML = `
-  <h3>${title}</h3>
-  <button class="close-btn">X</button>
-  <div class="create-list">
-  <label for="id">書籍編號</label>
-  <input class="book_id" type="text" placeholder="id">
-  <label for="name">書籍名稱</label>
-  <input class="book_name" type="text" placeholder="name">
-  <label for="author">書籍作者</label>
-  <input class="book_author" type="text" placeholder="author">
-  <label for="classification">書籍分類</label>
-  <input class="book_class" type="text" placeholder="classification">
-  <button class="create-btn">create</button>
-  </div>
-  `;
-  const create_btn = document.querySelector(".create-btn");
-  const close_btn = document.querySelector(".close-btn");
-  const result_Id = document.querySelector(".book_id").value;
-  const result_Name = document.querySelector(".book_name").value;
-  const result_Author = document.querySelector(".book_author").value;
-  const result_Class = document.querySelector(".book_class").value;
-
-  console.log(create_btn);
-  close_btn.addEventListener("click", () => {
-    overlay.classList.add("hidden");
-    create_container.classList.add("hidden");
+function isIsogram(str) {
+  const letter = new Set();
+  const lowerCaseStr = str.toLowerCase();
+  const result = lowerCaseStr.split('');
+  result.forEach((item) => {
+    letter.add(item);
   });
-  create_btn.addEventListener("click", () => {
-    console.log(result_Id);
-  });
-}
-createInfo.addEventListener("click", async () => {
-  CreateInfo("編輯資料");
-  overlay.classList.remove("hidden");
-});
-
-/**
- * @async
- * @function PerpageDisplayData 分頁顯示資料
- * @param {Number} page
- */
-async function PerpageDisplayAllBookData(page, table) {
-  try {
-    DisplayContainer.innerHTML = " ";
-    const response = await FetchApi(
-      `http://localhost:4050/api/page/${page}/${table}`,
-      "GET"
-    );
-    DisplayBookInfo(response);
-    console.log(response);
-  } catch (error) {
-    console.log(error);
+  if (letter.size === result.length) {
+    return true;
+  } else {
+    return false;
   }
 }
+console.log(isIsogram("moOse"));
+function getCount(str) {
+  const result = str.split("");
+  const kata = ["a", "e", "i", "o", "u"];
+  let count = 0;
 
-async function PerpageDisplayBookInfo(page, column, info) {
-  try {
-    DisplayContainer.innerHTML = " ";
-    const response = await FetchApi(
-      `http://localhost:4050/api/${column}/${info}/${page}`,
-      "GET"
-    );
-    DisplayBookInfo(response);
-    console.log(response);
-  } catch (error) {
-    console.log(error);
+  for (let i = 0; i < kata.length; i++) {
+    for (let j = 0; j < result.length; j++) {
+      if (result[j] === kata[i]) {
+        count += 1;
+      }
+    }
   }
+  return count;
 }
-/**
- * 讓分頁顯示資料功能在搜尋按鈕被點擊後才調用
- * 搜尋功能邏輯：
- * 1. if:如果選單內的數值為默認數值(代表沒有選取選項)且輸入匡數值為空 不能被搜尋
- * 2. else if:如果輸入匡內數值不為0 則調用“SelectInfoValue()”
- * 3. else if: 調用SelectClassificationValue()查詢書籍分類
- */
-search_btn.addEventListener("click", async () => {
-  DisplayLoading();
-  var result_option = SelectClassificationValue();
-  var selectedOption = FilterSelect.options[FilterSelect.selectedIndex].value;
-  const pages = ["page1", "page2", "page3", "page4"];
-  pages.forEach((pageItem, index) => {
-    pageItem.addEventListener("click", async () => {});
-  });
-});
+console.log(getCount("abracadara"));
 
-/**
- * @async
- * @function SelectOptionValue 取得選單的內容
- * @returns {string}
- */
-function SelectClassificationValue() {
-  var selectedOption =
-    Classification.options[Classification.selectedIndex].value;
+function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+  const FinalResult = dna.replace(/T/g, "U"); //replce只會替代掉第一個T 如果要替換掉全部的T要用正則表達式 /g代表全部
+  return FinalResult;
+}
 
-  // Use a switch statement to handle different classification values
-  switch (selectedOption) {
-    case "文學":
-      console.log("分類為文學");
-      break;
-    case "藝術":
-      console.log("分類為藝術");
-      break;
-    case "人文":
-      console.log("分類為人文");
-      break;
-    case "歷史":
-      console.log("分類為歷史");
-      break;
-    case "體育":
-      console.log("分類為體育");
-      break;
-    case "奇幻":
-      console.log("分類為奇幻");
-      break;
-    case "武俠":
-      console.log("分類為武俠");
-      break;
-    case "漫畫":
-      console.log("分類為漫畫");
-      break;
-    case "愛情":
-      console.log("分類為愛情");
-      break;
-    case "恐怖":
-      console.log("分類為恐怖");
-      break;
-    default:
-      console.log("未知分類");
+console.log(DNAtoRNA("UTTT"));
+
+const binaryArrayToNumber = (arr) => {
+  const Str = arr.join('');
+  const Final = parseInt(Str)
+  const result = Final.toString(2);
+  return parseInt(Str, 2)
+};
+console.log(binaryArrayToNumber([0, 0, 1, 0]));
+
+
+//降序排列
+function descendingOrder(str) {
+  const result = String(str).split('');
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < result.length; j++) {
+      if (Number(result[j + 1]) > Number(result[j])) {
+        const temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
   }
-  return selectedOption;
+  return Number(result.join(''));
 }
 
-/**
- * @async
- * @function SelectDifferentApi 查詢同一個分類下的所有書籍
- */
+//降序排列
+function descendingOrder1(str) {
+  const result = String(str).split('').sort().reverse().join('');
+  return Number(result);
+}
+console.log(descendingOrder1(564738291))
 
-async function SelectDifferentApi(option) {
-  try {
-    const response = await FetchApi(
-      `http://localhost:4050/api/classification/${option}`,
-      "GET"
-    );
-    DisplayBookInfo(response);
-  } catch (error) {
-    console.log(error);
+function digitize(n) {
+  const arr = String(n).split('').reverse();
+  const result = arr.map((item) => {
+    return Number(item);
+  })
+  return result
+}
+console.log(digitize(12243));
+
+
+
+function solution(str, ending) {
+  const str1 = str.split('');
+  const result1 = ending.split('');
+
+  // 反轉 str1，取出與 ending 長度相同的部分，然後再反轉回來，轉換成字符串
+  const subStr = str1.reverse().slice(0, result1.length).reverse().join('');
+
+  // 打印調試信息
+  console.log(subStr);
+  console.log(ending);
+
+  // 比較 subStr 和 ending 是否相同，返回結果
+  return subStr === ending;
+
+}
+
+// 測試案例
+console.log(solution("hello", "lo")); // true
+console.log(solution("hello", "ell")); // false
+console.log(solution("hello", "hello")); // true
+console.log(solution("hello", "o")); // true
+console.log(solution("hello", "")); // true
+
+console.log(solution("hello", "lo"))
+
+function disemvowel(str) {
+  return str.toLowerCase().replace(/[aeiou]/g, "");
+}
+
+console.log(disemvowel("This website is for losers LOL!"))
+
+function SeriesSum(n) {
+  let result = 0;
+  for (let i = 0; i < n; i++) {
+    result += 1 / (1 + i * 3)
   }
+  return result.toFixed(2)
 }
+console.log(SeriesSum(3))
 
-test_borrow.addEventListener("click", async () => {
-  SearchBorrowRecord();
-});
-/**
- * 根據書籍編號 書籍作者 書籍名稱查詢
- * @function SelectInfoValue
- */
-async function SelectInfoValue(value, page) {
-  var selectedOption = FilterSelect.options[FilterSelect.selectedIndex].value;
-  if (selectedOption === "書籍編號" && typeof FilterInput.value == "number") {
-    const response = FetchApi(
-      `http://localhost:4050/api/book_id/${value}/${page}`,
-      "GET"
-    );
-    return response;
-  } else if (
-    selectedOption === "書籍名稱" &&
-    typeof FilterInput.value == "string"
-  ) {
-    const response = FetchApi(
-      `http://localhost:4050/api/book_name/${value}/${page}`,
-      "GET"
-    );
-    return response;
-  } else if (
-    selectedOption === "書籍作者" &&
-    typeof FilterInput.value == "string"
-  ) {
-    const response = FetchApi(
-      `http://localhost:4050/api/author_name/${value}/${page}`,
-      "GET"
-    );
-    return response;
+function sumTwoSmallestNumbers(numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+      if (numbers[j] > numbers[j + 1]) {
+        const temp = numbers[j + 1];
+        numbers[j + 1] = numbers[j];
+        numbers[j] = temp;
+      }
+    }
   }
+  return numbers[0] + numbers[1];
 }
 
-/**
- * @async
- * @function SearchBorrowRecord 查詢指定書籍的借閱紀錄
- */
-async function SearchBorrowRecord() {
-  try {
-    const response = await FetchApi(
-      "http://localhost:4050/api/borrow_record",
-      "GET"
-    );
-    DisplayBorrowInfo(response);
-  } catch (error) {
-    console.log(error);
+console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]))
+
+function accum(str) {
+  const result = str.split('');
+  let finalArray = [];
+  let count = 0;
+  for (let i = 0; i < result.length; i++) {
+    count += 1
+    let str1 = ''
+    for (j = 0; j < count; j++) {
+      if (j = 0) {
+        str1 += result[i].toUpperCase();
+      }
+      str1 += result[i].toLowerCase()
+    }
+    finalArray.push(str1);
   }
+  console.log(finalArray)
 }
-
-function createUID(max) {
-  const random = Math.floor(Math.random() * max);
-  return random;
+let str1 = '';
+for (let i = 0; i < 4; i++) {
+  const str = 'i';
+  console.log(str1 += str);
 }
+console.log(accum("ZpglnRxqenU"))
