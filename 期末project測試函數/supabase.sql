9444b0b6086bff7e75d7cfa9f5b37336df4772c1
white <haoweizhou891@gmail.com>
2024-10-15 12:28:05 +0800

    feat: for data analysis document

--- 期末project測試函數/supabase.sql
+++
@@ -1,4 +0,0 @@
-Create table BooksData (book_id varchar(50), book_name varchar(50),author_name varchar(50),Classification varchar(50));
-
-
-INSERT INTO booksdata(book_id,book_name,author_name,classification)vault(1,'mathbook','henry','math')
