dab129c69b8375f2a11e2f3f33e3cfed3ee98df6
white <haoweizhou891@gmail.com>
2024-10-30 11:48:24 +0800

    update server.js

--- GraphQL/database_local.js
+++ GraphQL/database_local.js
@@ -9,7 +9,6 @@ const pool = new Pool({
          user: process.env.POSTGRES_ENV === 'CLOUD' ? process.env.CLOUD_POSTGRES_USER : process.env.LOCAL_POSTGRES_USER,
          password: process.env.POSTGRES_ENV === 'CLOUD' ? process.env.CLOUD_POSTGRES_PASSWORD : process.env.LOCAL_POSTGRES_PASSWORD,
          database: process.env.POSTGRES_ENV === 'CLOUD' ? process.env.CLOUD_POSTGRES_DATABASE : process.env.LOCAL_POSTGRES_DATABASE,
-         connectionString: process.env.CLOUD_DATABASE_URL
 });
 
 
--- GraphQL/server.js
+++ GraphQL/server.js
@@ -1,27 +1,64 @@
 const db = require('./database_local')
+const { Pool } = require('pg')
 const express = require('express')
 const { ApolloServer } = require('apollo-server-express')
 const userTypeDefs = require('./schema/user')
 const resolvers = require('./resolvers/queryResolvers');
-
-db.connect().then(() => {
-         console.log('Connect to the database')
-}).catch(error => {
-         console.log('Connect error:', error.stack)
-         throw new Error('Database connect error')
+const pool = new Pool({
+         connectionString: process.env.CLOUD_DATABASE_URL
 })
 
+function ConnectDatabase() {
+         if (process.env.POSTGRES_ENV === 'LOCAL') {
+                  db.connect().then(() => {
+                           console.log('Connect to the LocalHost database')
+                  }).catch(error => {
+                           console.log('Connect error:', error.stack)
+                           throw new Error('Database connect error')
+                  })
+         } else {
+                  pool.connect().then(() => {
+                           console.log('Connect to the Cloud PostgreSQL database')
+                  }).catch(error => {
+                           console.log('Connect error:', error.stack)
+                           throw new Error('Database connect error')
+                  })
+         }
+}
+ConnectDatabase()
+
+
+let server;
 const port = process.env.PORT || 4000
 async function startServer() {
-         const server = new ApolloServer({
+
+         server = new ApolloServer({
                   typeDefs: [userTypeDefs],
                   resolvers: resolvers,
                   cache: 'bounded',
                   persistedQueries: {
                            ttl: 900
                   },
-                  context: () => ({ db })
+                  context: () => {
+                           if (process.env.POSTGRES_ENV === 'LOCAL') {
+                                    return { db }
+                           } else {
+                                    return { pool }
+                           }
+                  }
          })
+
+         server = new ApolloServer({
+                  typeDefs: [userTypeDefs],
+                  resolvers: resolvers,
+                  cache: 'bounded',
+                  persistedQueries: {
+                           ttl: 900
+                  },
+                  context: () => ({ pool })
+         })
+
+
 
          await server.start();
          const app = express()
