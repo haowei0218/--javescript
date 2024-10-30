e2ec8d7518fa9aa4df63570fab415d96e1066c05
white <haoweizhou891@gmail.com>
2024-10-30 11:19:19 +0800

    feat:update env and database_local.js

--- GraphQL/database_local.js
+++ GraphQL/database_local.js
@@ -9,7 +9,7 @@ const pool = new Pool({
          user: process.env.POSTGRES_ENV === 'CLOUD' ? process.env.CLOUD_POSTGRES_USER : process.env.LOCAL_POSTGRES_USER,
          password: process.env.POSTGRES_ENV === 'CLOUD' ? process.env.CLOUD_POSTGRES_PASSWORD : process.env.LOCAL_POSTGRES_PASSWORD,
          database: process.env.POSTGRES_ENV === 'CLOUD' ? process.env.CLOUD_POSTGRES_DATABASE : process.env.LOCAL_POSTGRES_DATABASE,
-         ssl: process.env.POSTGRES_ENV === 'CLOUD' ? { rejectUnauthorized: false } : false,
+         connectionString: process.env.CLOUD_DATABASE_URL
 });
 
 
